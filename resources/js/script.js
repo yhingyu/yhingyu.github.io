5b4a0fc8389ac460a210a392c29d00831cc13ad1
yhingyu <edixony@gmail.com>
2017-09-28 21:17:25 +0800

    Added mobile navigation and generated favicon

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,5 @@
 /*jslint browser: true*/
-/*global $, jQuery, alert*/
+/*global $, document, location */
 
 $(document).ready(function () {
 
@@ -62,5 +62,47 @@ $(document).ready(function () {
                 }
             }
         });
+
+
+    /* Animations on scroll */
+    $('.js--wp-1').waypoint(function () {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-2').waypoint(function () {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-3').waypoint(function () {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-4').waypoint(function () {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
+
+    /* Mobile navigation */
+    $('.js--nav-icon').click(function () {
+
+        var nav = $('.js--main-nav'), icon = $('.js--nav-icon i');
+
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+
+        nav.slideToggle(200);
+    });
 
 });
